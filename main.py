f83b903d44a5949b0e0898c66698fd50332a13a0
rommmmmka <rommmmmka.m@gmail.com>
2022-05-06 10:41:26 +0300

    file chooser added

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
+import os
 import webbrowser
 
-import validators
 from kivy.app import App
-from kivy.clock import Clock
 from kivy.core.clipboard import Clipboard
-from kivy.lang import Builder
-# from pyzbar.pyzbar import decode
-# from kivy_garden.zbarcam import ZBarCam
-from kivy.properties import StringProperty, BooleanProperty
-from kivy.uix.screenmanager import ScreenManager, Screen, RiseInTransition
+from kivy.core.window import Window
+from kivy.properties import BooleanProperty, StringProperty
+from kivy.uix.screenmanager import RiseInTransition, Screen, ScreenManager
+from PIL import Image
+from pyzbar import pyzbar
+import validators
 
 
 class Cam(Screen):
 
-    def show_result(self, text):
+    def get_result(self, text):
         if not text:
             return
         self.manager.get_screen('results').text = text
@@ -26,7 +26,14 @@ class Results(Screen):
 
     def on_enter(self):
         self.is_link = validators.url(self.text)
-        self.ids['action_btn'].text = 'Перейти по ссылке' if self.is_link else 'Скопировать текст'
+        action_btn = self.ids['action_btn']
+        action_btn.text = 'Перейти по ссылке' if self.is_link else 'Скопировать текст'
+        action_btn.size = 0, 0
+        if not self.text:
+            action_btn.size_hint = 0, 0
+            self.text = 'Ошибка! Текст не распознан'
+        else:
+            action_btn.size_hint = 1, 1
 
     def action(self):
         if self.is_link:
@@ -35,15 +42,26 @@ class Results(Screen):
             Clipboard.copy(self.text)
 
 
+class FileLoader(Screen):
+
+    def get_result(self, path, file):
+        image = Image.open(os.path.join(path, file[0]))
+        results = pyzbar.decode(image)
+        self.manager.get_screen('results').text = ', '.join([str(symbol.data.decode('UTF-8')) for symbol in results])
+        self.manager.current = 'results'
+
+
 class QrScannerApp(App):
-    # Builder.load_file("qrscanner.kv")
 
     def build(self):
+        Window.size = 640, 530
         self.sm = ScreenManager(transition=RiseInTransition())
         self.cam = Cam(name='cam')
         self.results = Results(name='results')
+        self.file_loader = FileLoader(name='file_loader')
         self.sm.add_widget(self.cam)
         self.sm.add_widget(self.results)
+        self.sm.add_widget(self.file_loader)
         return self.sm
 
 
